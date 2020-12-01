575d6e2253de364afb3eb1fcc80885ac9c18cb17
cxq1915495507 <36284879+cxq1915495507@users.noreply.github.com>
2020-12-02 01:46:15 +0900

    Update client.go

--- client/client.go
+++ client/client.go
@@ -14,6 +14,7 @@ import (
 	"io"
 	"net"
 	"os"
+	"time"
 )
 
 
@@ -25,9 +26,11 @@ func main() {
 		return
 	}
 	defer conn.Close()
-
+        time.Sleep(time.Second * 5)
 	//5.发送文件名给接收端
-
+        conn ,err := net.Dial("tcp","193.167.100.100:18089")
+	        time.Sleep(time.Second * 5)
+        conn ,err := net.Dial("tcp","193.167.100.100:18089")
 	buf := make([]byte, 4096)
 	n, err := conn.Read(buf)
 	if err != nil {
